72bc9f4986c7b37ef9fde357e7e0dd3515e62b61
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2021-10-25 11:25:11 -0700

    first commit

---
+++ myfirstcomponent/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../main.css'
+
+function Main() {
+    return (
+        <div className='main'>
+            
+            <div className='photo-section'></div>
+            <div className = 'stats-section'></div>
+        </div>
+    )
+}
+
+export default Main
+
+
